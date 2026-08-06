e8537f2772fd671861e32413bdac30797ae0fffa
Likitha <likithasuguna2006@gmail.com>
2026-08-06 22:17:16 +0530

    Add script to print numbers from 1 to 50

---
+++ 08. looping-with-numbers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Problem: Looping with Numbers
+# Description: Print the natural numbers from 1 to 50.
+
+# Print numbers from 1 to 50
+for ((i=1; i<=50; i++))
+do
+    echo "$i"
+done
