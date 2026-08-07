59a9fbf47871a0b5a705bcd8b6908a63a23441c7
Likitha <likithasuguna2006@gmail.com>
2026-08-07 23:22:35 +0530

    Delete 10. arithmetic-expressions.sh

--- 10. arithmetic-expressions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Problem: Arithmetic Operations
-# Description: Evaluate a mathematical expression and print the result to 3 decimal places.
-
-read expression
-
-printf "%.3f\n" "$(echo "$expression" | bc -l)"
